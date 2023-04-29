f90bb0687843d7bae4ab824abfb05d9f1f53525f
Prosen Biswas <webprosen@gmail.com>
2023-04-30 02:07:13 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAG6vyFeCK9BkL2u7-sUa0axPjl2k8YNf4",
+  authDomain: "the-news-dragon-455fc.firebaseapp.com",
+  projectId: "the-news-dragon-455fc",
+  storageBucket: "the-news-dragon-455fc.appspot.com",
+  messagingSenderId: "88898649321",
+  appId: "1:88898649321:web:8e11876d086d209cf64478"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <small>2023 Copyright. All Rights Reserved</small>
+        </div>
+    );
+};
+
+export default Footer;
